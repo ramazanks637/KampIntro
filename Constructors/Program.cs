121d3a2e86b877da5a3973fbb6344f9a3ee013e8
Ramazan Köse <ramazanks637@gmail.com>
2022-08-13 19:36:49 +0300

    kodlama.io ödevler alıştırmalar yapıldı

---
+++ Constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer {Id=1,FirstName="Engin",LastName="Demiroğ",City="Ankara" };
+
+
+
+            Customer customer2 = new Customer ( 2, "Derin", "Demiroğ", "Ankara" );
+
+
+
+
+        }
+    }
+    class Customer
+    {
+        public Customer() // default constructor parametresi olmayan constructor
+        {
+
+        }
+        
+        public Customer(int Id,string firstName,string lastName,string city)
+        {
+            //Console.WriteLine("Yapıcı blok çalıştı");
+
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
